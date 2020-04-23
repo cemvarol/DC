d1db1947316cb353df1bffd3d236fab4255db527
cemvarol <cem.varol@outlook.com>
2020-04-23 07:52:20 +0100

    Add files via upload

---
+++ ChromeInstall.ps1
@@ -0,0 +1,5 @@
+$url = "https://https://github.com/cemvarol/DC/blob/master/ChromeSetup.exe?raw=true"
+$output = "$env:USERPROFILE\downloads\ChromeSetup.exe"
+Invoke-WebRequest -Uri $url -OutFile $output
+
+& "$env:USERPROFILE\downloads\ChromeSetup.exe"
